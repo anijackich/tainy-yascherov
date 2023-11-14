cb4f32a869707c6c1798df9e87e38bb3e2426c21
anijackich <anijackich@gmail.com>
2023-11-14 12:29:01 +0300

    целковый коммит

---
+++ змей_крест_крест.h
@@ -0,0 +1,99 @@
+#ifndef Болото
+#define Болото
+
+#ifdef _WIN32
+#include <Windows.h>
+#define разуметь_славянскую_молву SetConsoleOutputCP(1251)
+
+#endif
+
+#include <fstream>
+#include <iostream>
+
+
+#define главный_змей_трёхглавый int main
+#define внедрить using
+#define Атлантида std
+#define прошипеть cout
+#define расслышать cin
+#define отдать return
+
+#define ползти do
+#define доколе while
+#define каждому for
+
+#define коли if
+#define отнюдь else
+
+#define отряд class
+#define стая struct
+#define перепись enum
+
+#define бить_славян break
+#define добить_славян continue
+#define путевой_камень switch
+#define путь case
+#define базированно default
+
+#define сладкие_речи true
+#define подлая_ложь false
+
+#define перескочить endl
+#define новь new
+#define казнь delete
+#define на_дно_болота nullptr
+#define пустота NULL
+#define авось random
+#define вожак_приказал const
+#define степень pow
+#define отбить_удар_руса throw
+#define стычка_с_русом exception
+#define подсмотреть_писания_русов ifstream
+#define переписать_историю ofstream
+#define летопись fstream
+#define змееподобный friend
+#define сбросить_чешую goto
+#define свет_мой_зеркальце template
+#define скажи typename
+#define личный private
+#define общий public
+#define на_виду protected
+#define встрой inline
+#define кощей static
+#define однохвостый unsigned
+#define допытывать_руса try
+#define подловить_руса catch
+#define пещера namespace
+#define откупорить open
+#define закупорить close
+#define на_авось auto
+#define аки static_cast
+#define ухватить peek
+
+#define зачерпнуть getline
+#define ящер_брат good
+#define рус_противный fail
+#define вот_и_сказочке_конец eof
+#define вск eof
+#define берег_болота ostream
+#define устье_болота istream
+#define подменяло std::swap
+#define откусить get
+
+внедрить целый = int;
+внедрить дробный = double;
+внедрить слабодробный = float;
+внедрить закорючка = char;
+внедрить каракули_славянские = Атлантида::string;
+внедрить не_пойми_кто = void;
+внедрить малек = short;
+внедрить длинный_змей = long;
+внедрить самый_длинный_змей = long long;
+внедрить мерило = size_t;
+внедрить розсуд = bool;
+
+
+перепись счёт_древних_шизов{ ноль = 0, целковый = 1, чекушка = 2, порнушка = 3, пердушка = 4,
+    засерушка = 5, жучок = 6, мудачок = 7, воротничок = 8, дурачок = 9 };
+
+#endif
